8cf66539f8778a3d6db474d12ee2558837c82476
Luchian Gabriel <93213450+luchiangabriel@users.noreply.github.com>
2022-03-24 22:39:55 +0200

    Delete impartire.py

--- impartire.py
+++
@@ -1,6 +0,0 @@
-x = int(input("Valoarea primului element: "))
-y = int(input("Valoarea al doilea element: "))
-sum = x + y;
-imp = int(input("Impartim la: "))
-rezultat= sum/imp
-print ("Impartirea este: ", rezultat)
